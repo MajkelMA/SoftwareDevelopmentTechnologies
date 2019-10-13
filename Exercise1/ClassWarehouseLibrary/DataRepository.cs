f6e866b3a983b8c432c0bc015947c08f65754781
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-10-13 23:44:34 +0200

    Add CRUD operations in DataRepository class for client

--- Exercise1/ClassWarehouseLibrary/DataRepository.cs
+++ Exercise1/ClassWarehouseLibrary/DataRepository.cs
@@ -1,7 +1,39 @@
-﻿namespace ClassWarehouseLibrary
+﻿using System.Collections.Generic;
+
+namespace ClassWarehouseLibrary
 {
     class DataRepository
     {
-        private DataContext DataContext;
+        private DataContext _DataContext;
+
+        public DataRepository(DataContext DataContext)
+        {
+            _DataContext = DataContext;
+        }
+
+        public void AddClient(Client client)
+        {
+            _DataContext.Clients.Add(client);
+        }
+
+        public Client GetClient(int id)
+        {
+            return _DataContext.Clients[id];
+        }
+
+        public List<Client> GetAllClients()
+        {
+            return _DataContext.Clients;
+        }
+
+        public void UpdateClient(Client newClientInfo, int clientId)
+        {
+            _DataContext.Clients[clientId] = newClientInfo;
+        }
+
+        public void DeleteClient(Client clientToDelete)
+        {
+            _DataContext.Clients.Remove(clientToDelete);
+        }
     }
 }
