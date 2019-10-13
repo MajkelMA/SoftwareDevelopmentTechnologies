9b2218b3c7248fac411a9f429c7d44b3c0d2201a
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-10-13 22:40:14 +0200

    Update DataContext.cs

--- Exercise1/ClassWarehouseLibrary/DataContext.cs
+++ Exercise1/ClassWarehouseLibrary/DataContext.cs
@@ -5,6 +5,16 @@ namespace ClassWarehouseLibrary
 {
     class DataContext
     {
-
+        public List<Client> Clients { get; set; }
+        public Dictionary<int, Product> Products { get; set; }
+        public ObservableCollection<Invoice> Invoices { get; set; }
+        public List<InventoryStatus> InventoryStatuses { get; set; }
+        public DataContext()
+        {
+            Clients = new List<Client>();
+            Products = new Dictionary<int, Product>();
+            Invoices = new ObservableCollection<Invoice>();
+            InventoryStatuses = new List<InventoryStatus>();
+        }
     }
 }
