6bff2fa7907d21dca2d9ec971990c6ed53e6a1a6
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-10-23 22:16:25 +0200

    Update DataService.cs

--- Exercise1/ClassWarehouseLibrary/DataService.cs
+++ Exercise1/ClassWarehouseLibrary/DataService.cs
@@ -1,7 +1,34 @@
-﻿namespace ClassWarehouseLibrary
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ClassWarehouseLibrary
 {
     class DataService
     {
+        private IDataRepository _dataRepository;
 
+        //Product
+        Dictionary<int, Product> GetAllProducts()
+        {
+            return _dataRepository.GetAllProducts();
+        }
+
+        //Clients
+        List<Client> GetAllClients()
+        {
+            return _dataRepository.GetAllClients();
+        }
+
+        //Invoices
+        ObservableCollection<Invoice> GetAllInvoices()
+        {
+            return _dataRepository.GetAllInvoices();
+        }
+
+        //InventoryStatuses
+        List<InventoryStatus> GetInventoryStatuses()
+        {
+            return _dataRepository.GetAllInventoryStatuses();
+        }
     }
 }
