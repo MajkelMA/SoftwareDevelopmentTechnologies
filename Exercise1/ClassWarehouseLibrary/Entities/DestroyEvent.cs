458cf5c09d15308083c996d829582eefc4f38d87
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-11-08 09:40:42 +0100

    Revert "Revert "test""
    This reverts commit 8b207d7ae3d14de386a389d64e80780c738e0375.

---
+++ Exercise1/ClassWarehouseLibrary/Entities/DestroyEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWarehouseLibrary.Entities
+{
+    class DestroyEvent : Event
+    {
+        public DestroyEvent(String description) : base(null, description)
+        {
+
+        }
+    }
+}
