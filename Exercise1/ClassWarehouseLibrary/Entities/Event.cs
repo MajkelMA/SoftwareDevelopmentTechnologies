a1935097a4d3a806c9e6d218ed5698e0980789a8
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-11-08 09:39:44 +0100

    test

---
+++ Exercise1/ClassWarehouseLibrary/Entities/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassWarehouseLibrary.Entities
+{
+    public abstract class Event
+    {
+        public Invoice Invoice { get; set; }
+        public String Description { get; set; }
+        
+        public Event(Invoice invoice, String description)
+        {
+            this.Invoice = invoice;
+            this.Description = description;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   EqualityComparer<Invoice>.Default.Equals(Invoice, @event.Invoice) &&
+                   Description == @event.Description;
+        }
+    }
+}
