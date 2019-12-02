0f4f01680c3b20d4894814479ee73c2039f84f4c
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-12-02 01:27:49 +0100

    add DataService class

---
+++ Exercise3/LINQ/DataService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class DataService
+    {
+        private static TablesDataContext DataContext = new TablesDataContext();
+        public static List<Product> GetProductsByName(string namePart)
+        {
+            Table<Product> products = DataContext.GetTable<Product>();
+            List<Product> result = (from product in products 
+                                    where product.Name.Contains(namePart)
+                                    select product).ToList();
+            return result;
+        }
+
+        public static List<Product> GetProductByVendorName(string vendorName)
+        {
+            Table<ProductVendor> productVendors = DataContext.GetTable<ProductVendor>();
+            List<Product> result = (from productVendor in productVendors
+                                    where productVendor.Vendor.Name.Equals(vendorName)
+                                    select productVendor.Product).ToList();
+            return result;
+        }
+
+        public static List<string> GetProductNamesByVendorName(string vendorName)
+        {
+            Table<ProductVendor> productVendors = DataContext.GetTable<ProductVendor>();
+            List<string> result = (from productVendor in productVendors
+                                   where productVendor.Vendor.Name.Equals(vendorName)
+                                   select productVendor.Product.Name).ToList();
+            return result;
+        }
+
+        public static string GetProductVendorByProductName(string productName)
+        {
+            //dla michała
+            return null;
+        }
+
+        public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
+        {
+            Table<ProductReview> productReviews = DataContext.GetTable<ProductReview>();
+
+            List<Product> result = (from productReview in productReviews
+                                    orderby productReview.ReviewDate descending
+                                    select productReview.Product).Take(howManyReviews).ToList();
+
+            return result.Distinct().ToList();
+        }
+
+        public static List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
+        {
+            //dla michała
+            return null;
+        }
+
+        public static List<Product> GetNProductFromCategory(string categoryName, int n)
+        {
+            Table<Product> products = DataContext.GetTable<Product>();
+            List<Product> result = (from product in products
+                                    orderby product.Name
+                                    where product.ProductSubcategory.ProductCategory.Name.Equals(categoryName)
+                                    select product).Take(n).ToList();
+            return result;
+        }
+
+        public static int GetTotalStandardCostByCategory(ProductCategory category)
+        {
+            //dla michała
+            return 0;
+        }
+    }
+}
---
+++ Exercise3/Tests/LINQTest.cs
@@ -0,0 +1,39 @@
+﻿using LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class LINQTest
+    {
+        [TestMethod]
+        public void GetProductsByNameTest()
+        {
+            List<Product> products = DataService.GetProductsByName("Women's");
+            List<Product> products2 = DataService.GetProductsByName("Touring-3000 Yellow, 50");
+            List<Product> products3 = DataService.GetProductsByName("test");
+            Assert.AreEqual(6, products.Count);
+            Assert.AreEqual(1, products2.Count);
+            Assert.AreEqual(0, products3.Count);
+        }
+
+        [TestMethod]
+        public void GetProductByVendorNameTest()
+        {
+            List<Product> products = DataService.GetProductByVendorName("Comfort Road Bicycles");
+            List<Product> products2 = DataService.GetProductByVendorName("");
+            Assert.AreEqual(2, products.Count);
+            Assert.AreEqual(0, products2.Count);
+        }
+
+        [TestMethod]
+        public void GetProductsWithNRecentReviewsTest()
+        {
+            List<Product> products = DataService.GetProductsWithNRecentReviews(4);
+            List<Product> products2 = DataService.GetProductsWithNRecentReviews(0);
+            Assert.AreEqual(3, products.Count);
+            Assert.AreEqual(0, products2.Count);
+        }
+    }
+}
