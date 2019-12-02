720f3f937abe5cbfe965a7cac74044dd6ea092d7
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-12-02 02:05:44 +0100

    add ExtensionMethods class

---
+++ Exercise3/LINQ/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public static class ExtensionMethods
+    {
+        public static List<Product> GetProductsWithoutCategory_QuerySyntax(this List<Product> products)
+        {
+            List<Product> result = (from product in products
+                                    where product.ProductSubcategory == null
+                                    select product).ToList();
+            return result;
+        }
+
+        public static List<Product> GetProductsWithoutCategory_MethodSyntax(this List<Product> products)
+        {
+            return products.Where(product => product.ProductSubcategory == null).ToList();
+        }
+    }
+}
