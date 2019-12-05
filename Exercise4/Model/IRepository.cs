07a927de54179986c7016af867c91fdb7e39d7af
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-12-06 00:09:59 +0100

    init

---
+++ Exercise4/Model/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    interface IRepository<T>
+    {
+        bool Add(T item);
+        bool Delete(string id);
+        T Get(string id);
+        void Update(T item);
+    }
+}
