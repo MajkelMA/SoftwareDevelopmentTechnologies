4cffcb54c9f3cdc744ec1a6f82d43ca644b0a7fd
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-12-14 21:42:32 +0100

    add model layer

---
+++ Exercise4/Model/DataContext.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class DataContext : IDataContext<Product>
+    {
+        private readonly TablesDataContext tables;
+
+        public DataContext(TablesDataContext tables)
+        {
+            this.tables = tables;
+        }
+
+        public bool Add(Product item)
+        {
+            try
+            {
+                tables.GetTable<Product>().InsertOnSubmit(item);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(Product item)
+        {
+            try
+            {
+                tables.GetTable<Product>().DeleteOnSubmit(item);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Product Get(int id)
+        {
+            return (from product in tables.GetTable<Product>()
+                    where product.ProductID == id
+                    select product).First();
+        }
+
+        public IQueryable<TTable> GetItems<TTable>() where TTable : class
+        {
+            return tables.GetTable<TTable>();
+        }
+
+        public bool Update(Product item)
+        {
+            try
+            {
+                Product productToUpdate = tables.GetTable<Product>().Where(p => p.ProductID == item.ProductID).First();
+                productToUpdate.Name = item.Name;
+                productToUpdate.ProductNumber = item.ProductNumber;
+                productToUpdate.Color = item.Color;
+                productToUpdate.SafetyStockLevel = item.SafetyStockLevel;
+                productToUpdate.ReorderPoint = item.ReorderPoint;
+                productToUpdate.StandardCost = item.StandardCost;
+                productToUpdate.ListPrice = item.ListPrice;
+                productToUpdate.Size = item.Size;
+                productToUpdate.SizeUnitMeasureCode = item.SizeUnitMeasureCode;
+                productToUpdate.WeightUnitMeasureCode = item.WeightUnitMeasureCode;
+                productToUpdate.Weight = item.Weight;
+                productToUpdate.DaysToManufacture = item.DaysToManufacture;
+                productToUpdate.ProductLine = item.ProductLine;
+                productToUpdate.Class = item.Class;
+                productToUpdate.Style = item.Style;
+                productToUpdate.ProductSubcategoryID = item.ProductSubcategoryID;
+                productToUpdate.SellStartDate = item.SellStartDate;
+                productToUpdate.ProductModelID = item.ProductModelID;
+                productToUpdate.SellStartDate = item.SellStartDate;
+                productToUpdate.SellEndDate = item.SellEndDate;
+                productToUpdate.DiscontinuedDate = item.DiscontinuedDate;
+                productToUpdate.ModifiedDate = item.ModifiedDate;
+                tables.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- Exercise4/Model/IDataContext.cs
+++ Exercise4/Model/IDataContext.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model
 {
-    interface IRepository<T>
+    interface IDataContext<T>
     {
+        IQueryable<T> GetItems();
         bool Add(T item);
         bool Delete(T item);
         bool Update(T item);
         T Get(int id);
-        IQueryable<T> GetAll();
     }
 }
