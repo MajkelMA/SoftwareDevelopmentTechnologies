bde7daad797c115888d4fe048e81da35ab28c04d
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-12-15 11:20:52 +0100

    Basic view

---
+++ Exercise4/View/Interfaces/MainView.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using ViewModel;
+
+namespace View.Interfaces
+{
+    public class MainView : IWindow
+    {
+        
+
+
+        public void show()
+        {
+            AddProductWindow addProductWindow = new AddProductWindow();
+            addProductWindow.Show();
+        }
+    }
+}
---
+++ Exercise4/ViewModel/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand AddProductCommand { get; private set; }
+        public Lazy<IWindow> ChildWindow { get; set; }
+
+
+        public MainViewModel()
+        {
+            //this.AddProductWindow() += PropertyChanged;
+        }
+
+        private void AddProductWindow()
+        {
+            IWindow _child = ChildWindow.Value;
+            _child.show();
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
