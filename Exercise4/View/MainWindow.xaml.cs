eaefbaa1f7f04540f8397492358794edb91dbacb
MajkelMA <50554203+MajkelMA@users.noreply.github.com>
2019-12-18 23:38:21 +0100

    Update main window

--- Exercise4/View/MainWindow.xaml.cs
+++ Exercise4/View/MainWindow.xaml.cs
@@ -3,13 +3,14 @@ using System.Windows;
 using System.Windows.Input;
 using View.InterfaceImplementations;
 using ViewModel;
+using ViewModel.Interfaces;
 
 namespace View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IMyPopup
     {
         public MainWindow()
         {
@@ -37,6 +38,11 @@ namespace View
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             ((MainViewModel)DataContext).DeleteProductCommand.Execute(null);
+        }
+
+        public void ShowPopup(string message)
+        {
+            MessageBox.Show(message);
         }
     }
 }
